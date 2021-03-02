b60af7fbc40400ce6dd0f407f7d04449e228364f
Patrick Tuszakowski <mrtall500@gmail.com>
2021-03-02 12:27:58 -0500

    Stuck on delete

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 const ramenDiv = document.querySelector('div#ramen-menu')
 const ramenDetail = document.querySelector('div#ramen-detail')
 const ramenForm = document.querySelector('form#ramen-rating')
+const newRamen = document.querySelector('form#new-ramen')
+
 
 function loadOneRamen(ramenObj){
 
@@ -16,7 +18,36 @@ function loadOneRamen(ramenObj){
 function loadAllRamen(){ 
     fetch('http://localhost:3000/ramens')
         .then(res => res.json())
-        .then( ramens => ramens.forEach( ramen => loadOneRamen(ramen)))
+        .then( ramens => {
+            ramens.forEach( ramen => loadOneRamen(ramen))
+            
+            ramenDetail.innerHTML = `
+            <img class="detail-image" src=${ramens[0].image} alt=${ramens[0].name} />
+            <h2 class="name">${ramens[0].name}</h2>
+            <h3 class="restaurant">${ramens[0].restaurant}</h3>`
+
+            ramenForm.innerHTML = `
+            <label for="rating">Rating: </label>
+            <input type="text" name="rating" id="rating" value=${ramens[0].rating} />
+            <label for="comment">Comment: </label>
+            <textarea name="comment" id="comment">${ramens[0].comment}</textarea>
+            <input type="submit" value="Update" />
+            <button type="button" class = "delete" id = ${ramens[0].id}>Delete Me!</button>`
+            
+            const deleteBut = document.querySelector('button.delete')
+            deleteBut.addEventListener('click', function (event){
+                event.preventDefault()
+                
+                    fetch(`http://localhost:3000/ramens/${event.target.dataset.id}`, {
+                     method: 'DELETE',
+                        
+                        })
+    
+                            .then(res => res.json())
+                            .then(ramenObj => ramenObj.remove())
+
+})
+        })
 
 }
 
@@ -39,8 +70,10 @@ ramenDiv.addEventListener('click', function (event) {
             <input type="text" name="rating" id="rating" value=${ramenObj.rating} />
             <label for="comment">Comment: </label>
             <textarea name="comment" id="comment">${ramenObj.comment}</textarea>
-            <input type="submit" value="Update" />`
-        
+            <input type="submit" value="Update" />
+            <button type="button" class = "delete" id= ${ramenObj.id}>Delete Me!</button>`
+            
+
         })
 
     
@@ -74,5 +107,27 @@ ramenForm.addEventListener('submit', function (event){
 
 })
 
+newRamen.addEventListener('submit',function (event){
+    event.preventDefault()
+    const name = event.target.name.value
+    const rating = event.target.rating.value
+    const comment = event.target[4].value
+    const restaurant = event.target.restaurant.value
+    const image = event.target.image.value
+    const ramenNew = {name, rating, comment,restaurant, image}
+
+    fetch(`http://localhost:3000/ramens`, {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Accept': 'accplication/json'},
+        body: JSON.stringify(ramenNew)
+    })
+    .then( res => res.json())
+    .then(ramenObj => loadOneRamen(ramenObj))
+    event.target.reset()
+})
+
 
 loadAllRamen()
+
